429c678bd59096f8e9b3d94faca00ab64f2b6e47
Tobiasz Bartkowski <t.bartkowski@wp.pl>
2018-01-20 10:32:51 +0100

    Wczytywanie maksymalnej liczny wątków z pliku

--- proj.cs
+++ proj.cs
@@ -1,7 +1,32 @@
+using System;
+using System.IO;
+
 class Program
 {
+    public static int MaxLiczbaWatkow()
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader("MaxLiczbaWatkow.txt"))
+            {
+                string line = sr.ReadLine();
+                int maxLiczbaWatkow = int.Parse(line);
+                return maxLiczbaWatkow;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Problem z odczytem pliku");
+            Console.WriteLine(e.Message);
+        }
+        //wartość domyślna
+        return 5;
+    }
+
     static void Main()
     {
         System.Console.WriteLine("projekt1");
+        int maxLiczbaWatkow = MaxLiczbaWatkow();
+        Console.WriteLine(maxLiczbaWatkow);
     }
 }
